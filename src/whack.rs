90504f877c82310b3d290d403e8c372342b8278a
stroucki <stroucki@andrew.cmu.edu>
2026-08-01 15:07:40 -0400

    Enable word-sized match-extension (u64) in whackmatch for faster match extension, unconditional

--- src/whack.rs
+++ src/whack.rs
@@ -1,5 +1,6 @@
 // Copyright 2024-2026 by Michael Stroucken
 use crate::constants::*;
+use std::ptr;
 
 /// Compression dictionary
 ///
@@ -111,6 +112,32 @@ fn whackmatch(
         {
             candidate_match_position += 3;
             current_match_position += 3;
+
+            // Fast extension: compare u64 words where possible for speed, then finish byte-by-byte.
+            while current_match_position + 8 <= max_match_position
+                && candidate_match_position + 8 <= max_match_position
+            {
+                unsafe {
+                    let a = ptr::read_unaligned(src.as_ptr().add(current_match_position) as *const u64);
+                    let b = ptr::read_unaligned(src.as_ptr().add(candidate_match_position) as *const u64);
+                    if a == b {
+                        current_match_position += 8;
+                        candidate_match_position += 8;
+                        continue;
+                    }
+                    let diff = a ^ b;
+                    let byte_index = if cfg!(target_endian = "little") {
+                        (diff.trailing_zeros() / 8) as usize
+                    } else {
+                        (diff.leading_zeros() / 8) as usize
+                    };
+                    current_match_position += byte_index;
+                    // advance candidate position as well
+                    candidate_match_position += byte_index;
+                    break;
+                }
+            }
+
             while current_match_position < max_match_position {
                 if src[current_match_position] != src[candidate_match_position] {
                     break;
